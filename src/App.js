5039a2e019252b3ae1be50ae20f35b959ff50d98
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-06-25 17:51:21 -0500

    added event fire test for button greeting/ added new component Players to test data and props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state ={
+    greeting: ""
+  }
+  
   render() {
     return (
       <>
       <h1>Hello World</h1>
+      <button onClick={this.greet}>Greet</button>
+      <div>{this.state.greeting}</div>
       </>
     );
   }
+  
+  greet= () => {
+    this.setState({ greeting: "Hello there world"})
+  } 
+
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each'
 import 'jest-dom/extend-expect'
 
@@ -11,12 +11,20 @@ xit('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
+xit('renders without crashing', () => {
   render(<App/>)
 })
 
-it('reads hello world', () => {
+xit('reads hello world', () => {
    const { getByText } = render(<App />);
    const header = getByText(/Hello World/i)//regex ignores case
    expect(header).toBeVisible()
+})
+
+it('displays greeting when button is clicked', () => {
+  const { getByText } = render(<App />);
+  const button = getByText(/greet/i)
+  fireEvent.click(button)
+  getByText(/Hello there world/i)
+ 
 })
